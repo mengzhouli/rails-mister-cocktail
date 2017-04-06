e1c98f5f34f9baa2acbb880af216f3dc7fab4887
Mengzhou Li <mengzhou.li@gmail.com>
2017-04-06 14:12:22 -0400

    passed all rakes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+thing_serialized = open(url).read
+thing = JSON.parse(thing_serialized)
+
+thing["drinks"].each do |d|
+  Ingredient.create(name: d["strIngredient1"])
+end
+
