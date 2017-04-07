ae9dbf3b6b49d07bca53ecc1cc067f2219b4a35e
Mengzhou Li <mengzhou.li@gmail.com>
2017-04-07 13:47:36 -0400

    added banner and cards to front page

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,11 +1,10 @@
 class CocktailsController < ApplicationController
-
+  before_action :set_cocktail, except: [:index, :new, :create]
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
     @doses = @cocktail.doses
     @dose = Dose.new
   end
@@ -23,7 +22,24 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
 
   def cocktail_params
     params.require(:cocktail).permit(:name, photos: [])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Attachinary::Engine => "/attachinary"
-  resources :cocktails, except: [:edit, :update, :destroy] do
+  resources :cocktails, except: :destroy do
     resources :doses, only: :create
   end
 
